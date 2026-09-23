0deb973218da8475ae028ede54998e1e48f6e6b5
David Soliz Garcia <soliz.g.david@gmail.com>
2026-09-23 16:34:24 -0400

    Add browser setup and navigation

--- src/main/java/bookflight/BookFlightTest.java
+++ src/main/java/bookflight/BookFlightTest.java
@@ -1,14 +1,23 @@
 package bookflight;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BookFlightTest {
 
     private WebDriver driver;
 
-    public static void main (String[] args){
+    // Initialize the browser and navigate to the application
+    public void setUp() throws InterruptedException {
+
+        driver = new ChromeDriver();
+        driver.get("https://blazedemo.com/");
+        driver.manage().window().maximize();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
 
         BookFlightTest bookFlightTest = new BookFlightTest();
-
+        bookFlightTest.setUp();
     }
 }
